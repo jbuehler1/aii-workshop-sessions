eea7718b23307d9cb9b413fe0711409730abf8a2
jbuehler1 <95894408+jbuehler1@users.noreply.github.com>
2022-01-24 13:52:14 -0800

    Create 11.py

---
+++ 001.01 - Introduction to Programming/001.10 - Session 10/11.py
@@ -0,0 +1,40 @@
+color = "yellow"
+number = "5"
+sentence = f"the quick {color} fox jumped over {number} lazy dogs"
+print(sentence)
+
+x = "the quick brown fox jumps over the lazy dog"
+print(x)
+y= x.capitalize()
+print(y)
+
+x = "the quick brown fox jumps over the lazy dog"
+print(x)
+y= x.upper()
+print(y)
+
+x = "the quick brown fox jumps over the lazy dog"
+print(x)
+y= x.replace("brown", "red")
+print(y)
+
+x = "the quick brown fox jumps over the lazy dog"
+print(x)
+y= x.index('brown')
+print(y)
+
+#x = "the quick brown fox jumps over the lazy dog"
+#print(x)
+#y= x.rindex('brown')
+#print(y)
+
+x = "   the quick brown fox jumps over the lazy dog. \n"
+print(x)
+y= x.strip()
+print(y)
+
+x = "the quick brown fox jumps over the lazy dog"
+print(x)
+y= x[10:15]
+print(y)
+
